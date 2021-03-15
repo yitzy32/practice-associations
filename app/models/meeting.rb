b815827c057b47873ffa420c7f602c2a32c8de5c
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-15 17:33:20 -0400

    adds presence validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,9 @@
 class Meeting < ApplicationRecord
   has_many :meeting_speakers
   has_many :speakers, through: :meeting_speakers
+
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,7 @@
 class Speaker < ApplicationRecord
   has_many :meeting_speakers
   has_many :meetings, through: :meeting_speakers
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
